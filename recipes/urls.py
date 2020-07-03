25fb1680903dfbcb9596776e0b690fc24a8f8da1
avelyno <kou2295@calu.edu>
2020-07-03 02:15:06 -0400

    done

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from recipes import views
 
 urlpatterns = [
     path('', views.index),
-    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
-    path('author/<int:pk>/', views.author_detail, name='author_detail')
+    path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
+    path('author/<int:id>/', views.author_detail, name='author_detail')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,13 +3,16 @@ from recipes.models import Author, RecipeItems
 
 # Create your views here.
 def index(request):
-    data = RecipeItems.objects.all()
+    data = RecipeItems.objects.all() 
     return render(request, 'index.html', {'data': data})
 
-def recipe_detail(request, pk):
-    recipe = get_object_or_404(RecipeItems, pk=pk)
+def author_detail(request, id):
+    author = get_object_or_404(Author,id=id)
+    recipe = RecipeItems.objects.filter(author=id)
+    data = RecipeItems.objects.all()
+    return render(request, 'author_detail.html', {'author': author, 'recipe': recipe, 'data': data})
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(RecipeItems,id=id)
     return render(request, 'recipe_detail.html', {'recipe': recipe})
 
-def author_detail(request, pk):
-    author = get_object_or_404(RecipeItems, pk=pk)
-    return render(request, 'author_detail.html', {'recipe': author})
